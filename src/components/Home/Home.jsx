b37bd7af7eceef87f6ab4598c7be6a83840eae28
Tanim <abdullahtanim27@gmail.com>
2024-09-16 13:56:17 +0600

    tried 4th time to fix the layout shifting error

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,35 +6,28 @@ const Home = () => {
 	useEffect(() => {
 		const tl = gsap.timeline({ defaults: { ease: 'Power4.out' } })
 
+		tl.to('.intro', { opacity: 1, y: '0%', duration: 0.5 }) // Set intro visible and move it into view
 		tl.to('.text', {
 			y: '0%',
 			stagger: 0.1,
 			duration: 0.5,
 			ease: 'Power4.out',
 		})
+
 		tl.to('.slider', {
 			y: '-100%',
 			duration: 1,
 			delay: 0.5,
 			ease: 'power4.inOut',
 		})
+
 		tl.to('.intro', {
-			y: '-100%',
+			y: '-100%', // Move it off the screen smoothly
 			duration: 1,
 			ease: 'power2.out',
 		})
+
 		tl.fromTo('.hero', { opacity: 0 }, { opacity: 1, duration: 1 })
-
-		// Add a window resize event to adjust layout on orientation change
-		window.addEventListener('resize', () => {
-			tl.invalidate().restart() // Ensure animations reset on resize
-		})
-
-		return () => {
-			window.removeEventListener('resize', () => {
-				tl.invalidate().restart()
-			})
-		}
 	}, [])
 
 	return (
@@ -48,10 +41,10 @@ const Home = () => {
 				</p>
 			</div>
 
-			<div className='intro bg-black fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center'>
+			<div className='intro bg-black fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center opacity-0 translate-y-full'>
 				<div className='font-helveticaNeue font-bold text-5xl sm:text-7xl flex flex-col justify-center items-start'>
 					<h1 className='overflow-hidden'>
-						<span className='text translate-y-full inline-block pb-2'>
+						<span className='text translate-y-full inline-block pb-4'>
 							Your peak
 						</span>
 					</h1>
