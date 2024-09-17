a02c2d2a409dc0224550d6c61fcb3b1150f551ee
Tanim <abdullahtanim27@gmail.com>
2024-09-18 04:26:03 +0600

    changed the design in home page animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,83 +1,70 @@
 'use client'
 import gsap from 'gsap'
-import { useLayoutEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
-const Home = () => {
-	const introRef = useRef(null)
-	const sliderRef = useRef(null)
-	const mainRef = useRef(null)
+const About = () => {
+	useEffect(() => {
+		const tl = gsap.timeline({ defaults: { ease: 'Power3.out' } })
 
-	useLayoutEffect(() => {
-		const navbarHeight = mainRef.current.offsetHeight
-
-		const tl = gsap.timeline({ defaults: { ease: 'Power1.out' } })
-
-		tl.to('.text', {
-			y: '0%',
-			stagger: 0.15,
-			duration: 0.3,
-			// ease: 'Power1.out',
-		})
-
-		tl.to(sliderRef.current, {
-			y: `-${100 + (navbarHeight / window.innerHeight) * 100}%`,
-			duration: 1.5,
+		tl.to('.text1', {
+			opacity: 1,
+			duration: 0.5,
 			delay: 0.5,
-			ease: 'power2.inOut',
 		})
 
-		tl.to(introRef.current, {
-			y: `-${100 + (navbarHeight / window.innerHeight) * 100}%`,
-			duration: 2.5,
-			delay: 0.1,
-			ease: 'power1.out',
+		tl.to('.text2', {
+			opacity: 1,
+			duration: 0.5,
+			delay: 0.5,
 		})
 
-		tl.fromTo('.hero', { opacity: 0 }, { opacity: 1, duration: 0.8 }, '=-1')
+		tl.to('.text3', {
+			opacity: 1,
+			duration: 0.5,
+			delay: 0.5,
+		})
+
+		tl.to('.slider', {
+			y: '-100%',
+			duration: 1,
+			delay: 0.5,
+			ease: 'power4.inOut',
+		})
+
+		tl.to('.intro', {
+			y: '-100%',
+			duration: 1,
+			ease: 'power2.out',
+		})
+
+		tl.fromTo('.hero', { opacity: 0 }, { opacity: 1, duration: 0.8 })
 	}, [])
 
 	return (
 		<>
-			<main ref={mainRef}>
-				<div className='hero w-full h-[100vh] flex flex-col justify-center items-center '>
-					<div className='flex flex-col justify-center items-start'>
-						<h1 className='font-helveticaNeue font-bold text-5xl sm:text-7xl'>
-							SwiftPeak
-						</h1>
-						<p className='font-maison font-normal'>
-							This is Maison Neue SwiftPeak
-						</p>
-					</div>
+			<div className='w-full h-[100vh] flex justify-center items-center text-white'>
+				<div className='hero font-maison font-normal text-3xl sm:text-5xl'>
+					<h3 className=''>About Page</h3>
 				</div>
-			</main>
+			</div>
 
-			<div
-				ref={introRef}
-				className='intro fixed top-0 left-0 w-full h-full bg-black flex justify-center items-center'
-			>
-				<div className='introText font-helveticaNeue font-bold text-white text-5xl sm:text-7xl'>
-					<h1 className='hide overflow-hidden bg-black'>
-						<span className='text inline-block translate-y-full'>
-							Your Peak
-						</span>
+			<div className='intro bg-black fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center'>
+				<div className='font-helveticaNeue font-bold text-5xl sm:text-7xl flex flex-col justify-center items-start'>
+					<h1 className='overflow-hidden'>
+						<span className='text1 opacity-0'>Your Peak</span>
 					</h1>
-					<h1 className='hide overflow-hidden bg-black'>
-						<span className='text inline-block translate-y-full'>in</span>
+					<h1 className='overflow-hidden'>
+						<span className='text2 opacity-0'>in</span>
 					</h1>
-					<h1 className='hide overflow-hidden bg-black'>
-						<span className='text inline-block translate-y-full'>
-							Digital Solutions
-						</span>
+					<h1 className='overflow-hidden pb-4'>
+						<span className='text3 opacity-0'>Digital Solutions</span>
 					</h1>
 				</div>
 			</div>
 
-			<div
-				ref={sliderRef}
-				className='slider bg-white fixed top-0 left-0 w-full h-full bg-white translate-y-full'
-			></div>
+			<div className='slider bg-white fixed top-0 left-0 w-full h-full transform translate-y-full'></div>
 		</>
 	)
 }
 
-export default Home
+export default About
